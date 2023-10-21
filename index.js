182693ef8c891a810a12cb0051798fec25935aac
Taklit Dina <ez_taklit@esi.dz>
2023-10-21 13:48:27 +0100

    feat: File Metadata Microservice

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+const multer = require("multer");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
-
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+// Set up the storage for uploaded files
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  const file = req.file;
+  if (!file) return res.json({ error: "No file uploaded" });
+  const response = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  };
+  res.json(response);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
